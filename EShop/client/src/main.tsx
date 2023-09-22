3eeebe64dcdc3d17d56aaf4c8bf1a3dad8bcec0b
Demetrix98 <pawlowicz.k@wp.pl>
2023-09-22 20:26:15 +0200

    added the shopping cart feature

--- EShop/client/src/main.tsx
+++ EShop/client/src/main.tsx
@@ -6,12 +6,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes'
+import { StoreProvider } from './app/context/StoreContext';
 
 
 //import '../app/layout/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <StoreProvider>
+      <RouterProvider router={router}/>
+    </StoreProvider>
   </React.StrictMode>,
 )
